ac5f948aa80c7b623d27d992b1e708af561dc4e4
aimanmuzaffar95 <aimanmuzaffar95@gmail.com>
2021-04-03 16:25:05 +0500

    Changes: - Added a custom RatingView.kt - Added initial animation related code

---
+++ app/src/main/java/com/aiman/animatedrating/ratingview/BaseLayout.kt
@@ -0,0 +1,40 @@
+package com.aiman.animatedrating.ratingview
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatDelegate
+import com.aiman.animatedrating.R
+
+open class BaseLayout<T : Drawable> @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val cache: MutableMap<String, T>
+
+    init {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        context.theme.applyStyle(R.style.EmotionRatingTheme, false)
+        cache = mutableMapOf()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        cache.clear()
+    }
+
+    protected fun getDrawable(key: String, creator: () -> T?): T? {
+        return if (cache.containsKey(key)) {
+            cache[key]
+        } else {
+            val drawable = creator()
+            if (drawable != null) {
+                cache[key] = drawable
+            }
+            drawable
+        }
+    }
+}
--- app/src/main/java/com/aiman/animatedrating/ui/MainActivity.kt
+++ app/src/main/java/com/aiman/animatedrating/ui/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.aiman.animatedrating
+package com.aiman.animatedrating.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.aiman.animatedrating.R
+import com.aiman.animatedrating.databinding.ActivityMainBinding
+import com.aiman.animatedrating.databinding.ActivityMainBindingImpl
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: MainActivity
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
